3a3ff5e376880992044d0bc919d126b8a2405320
PierreWZK <pierredolivet@live.fr>
2021-12-15 17:01:31 +0100

    door Commands

---
+++ code/Levels/Origin/doorMiddle.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _ready():
+	pass
+
+#ALLER VERS LES LEVELS NORMAUX
+func _input(event):
+		if get_overlapping_bodies().size() > 0:
+			get_tree().change_scene("res://scene/Levels/Level1.tscn");
